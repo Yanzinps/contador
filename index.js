a475c29b35d72b54b538773de1a22c3fdcf2f7e8
Yan <122059882+Yanzinps@users.noreply.github.com>
2023-01-20 13:42:23 -0300

    commit 1 teste mtg contador

---
+++ index.js
@@ -0,0 +1,137 @@
+let contador1 = 0
+let contador2 = 0
+let contador3 = 0
+let contador4 = 0
+
+
+document.getElementById("menos1").onclick = function(){
+    contador1 -= 1;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+document.getElementById("reset1").onclick = function(){
+    contador1 = 40;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+document.getElementById("mais1").onclick = function(){
+    contador1 += 1;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+
+
+document.getElementById("menos51").onclick = function(){
+    contador1 -= 5;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+document.getElementById("zerar1").onclick = function(){
+    contador1 = 0;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+document.getElementById("mais51").onclick = function(){
+    contador1 += 5;
+    document.getElementById("cont1").innerHTML = contador1;
+}
+
+
+
+//-----------------------------primeiro contador
+
+document.getElementById("menos2").onclick = function(){
+    contador2 -= 1;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+
+document.getElementById("reset2").onclick = function(){
+    contador2 = 40;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+
+document.getElementById("mais2").onclick = function(){
+    contador2 += 1;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+
+
+document.getElementById("menos52").onclick = function(){
+    contador2 -= 5;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+
+document.getElementById("zerar2").onclick = function(){
+    contador2 = 0;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+
+document.getElementById("mais52").onclick = function(){
+    contador2 += 5;
+    document.getElementById("cont2").innerHTML = contador2;
+}
+//-----------------------------segundo contador
+
+document.getElementById("menos3").onclick = function(){
+    contador3 -= 1;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+document.getElementById("reset3").onclick = function(){
+    contador3 = 40;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+document.getElementById("mais3").onclick = function(){
+    contador3 += 1;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+
+document.getElementById("menos53").onclick = function(){
+    contador3 -= 5;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+document.getElementById("zerar3").onclick = function(){
+    contador3 = 0;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+document.getElementById("mais53").onclick = function(){
+    contador3 += 5;
+    document.getElementById("cont3").innerHTML = contador3;
+}
+
+//---------------------------terceiro contador
+
+document.getElementById("menos4").onclick = function(){
+    contador4 -= 1;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+
+document.getElementById("reset4").onclick = function(){
+    contador4 = 40;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+
+document.getElementById("mais4").onclick = function(){
+    contador4 += 1;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+
+document.getElementById("menos54").onclick = function(){
+    contador4 -= 5;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+
+document.getElementById("zerar4").onclick = function(){
+    contador4 = 0;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+
+document.getElementById("mais54").onclick = function(){
+    contador4 += 5;
+    document.getElementById("cont4").innerHTML = contador4;
+}
+//----------------------------quarto contador
